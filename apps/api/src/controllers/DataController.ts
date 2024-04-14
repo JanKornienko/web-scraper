8b97b0bd8f9d43efabab69dd6cae9c3cbd84860b
Jan Kornienko <jan.kornienko@icloud.com>
2024-04-14 23:49:20 +0200

    feat: getLinks

--- apps/api/src/controllers/DataController.ts
+++ apps/api/src/controllers/DataController.ts
@@ -6,27 +6,52 @@ import * as fs from 'fs';
 type Books = {
 	[key: string]: string;
 };
+async function getData(endpoint: string) {
+	try {
+		const baseUrl = 'https://books.toscrape.com/catalogue/';
+
+		const { data: response } = await axios.get(baseUrl + endpoint);
+		const $ = cheerio.load(response);
+
+		return $;
+	} catch (e) {
+		throw e;
+	}
+}
+
+async function getLinks(
+	endpoint: string = 'page-1.html',
+	links: string[] = []
+) {
+	try {
+		const $ = await getData(endpoint);
+
+		$('section > div > ol > li').each((i, book) => {
+			const link = $(book).find('h3 > a').attr('href');
+
+			if (link) {
+				links.push(link);
+			}
+		});
+
+		const nextEndpoint: string = $('ul.pager > li.next > a').attr('href') || '';
+
+		if (nextEndpoint === '') {
+			return links;
+		} else {
+			console.log(nextEndpoint);
+			return getLinks(nextEndpoint, links);
+		}
+	} catch (e) {
+		throw e;
+	}
+}
 
 export const create = async (req: Request, res: Response) => {
 	try {
-		const url = 'https://books.toscrape.com/index.html';
-		const books: Books = {};
+		const links = await getLinks();
 
-		const { data: response } = await axios.get(url);
-		const $ = cheerio.load(response);
-
-		const $data = $('section > div > ol > li').each((i, book) => {
-			const title = $(book).find('h3 > a').attr('title');
-			const price = $(book).find('p.price_color').text();
-
-			if (title) {
-				books[title] = price;
-			}
-
-			fs.writeFileSync('books.json', JSON.stringify(books));
-		});
-
-		res.status(201).json(books);
+		res.status(201).json(links);
 	} catch (e: any) {
 		const status = e.response ? e.response.status : 500;
 		res.status(status).json(e);
